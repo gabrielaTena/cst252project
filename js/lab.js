0bd54a21c3aad16ced5b68008349c24f85fbd09f
senava <78111849+senava@users.noreply.github.com>
2022-04-17 12:05:12 -0700

    04/17
    -landing page framework

--- js/lab.js
+++ js/lab.js
@@ -1,11 +1,21 @@
-new fullpage('#fullpage', {
-  sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
-});
+/**
+ * Author:  Stephanie Nava, Jason Matthew, Gabriela Tena
+ * Last edited:   04.05.2022
+ * License:   Public Domain
+ **/
 
-$('#getActiveSlide').click(function(){
-   console.log(fullpage_api.getActiveSlide());
-});
+// get element by id
+var scene = $('#scene').get(0);
+var parallaxInstance = new Parallax(scene);
 
-$('#getActiveSection').click(function(){
-   console.log(fullpage_api.getActiveSection());
-});
+// new fullpage('#fullpage', {
+//   sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+// });
+//
+// $('#getActiveSlide').click(function(){
+//    console.log(fullpage_api.getActiveSlide());
+// });
+//
+// $('#getActiveSection').click(function(){
+//    console.log(fullpage_api.getActiveSection());
+// });
