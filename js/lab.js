cf2c46afb3589b913d6a36a1ab18b2805a096d0e
senava <78111849+senava@users.noreply.github.com>
2022-04-17 20:23:44 -0700

    04/17
    -updated framework

--- js/lab.js
+++ js/lab.js
@@ -50,3 +50,17 @@ function complete(){
   clearInterval(timer);
   timer = null;
 }
+
+// create variable that triggers button to show text
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+$(".info").click(function(){
+  $(".information").toggle("slow");
+});
+
+$("#shawn-btn").click(function(){
+  $(".shawn-info").toggle("slow");
+});
